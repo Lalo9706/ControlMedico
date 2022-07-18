a663044bdf49bcffe5af8738d2bd1e142f06f868
Lalo9706 <antony_0000@hotmail.com>
2022-07-17 21:15:19 -0500

    ListView

--- ControlMedico/ControlMedico/Data/Repository/CitaRepository.cs
+++ ControlMedico/ControlMedico/Data/Repository/CitaRepository.cs
@@ -1,10 +1,16 @@
-﻿using System;
+﻿using ControlMedico.Data.Model;
+using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace ControlMedico.Data.Repository
 {
     internal class CitaRepository
     {
+        internal static Task<List<Cita>> RecuperarCitasMedico(int idUsuario)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
--- ControlMedico/ControlMedico/ViewModel/PrincipalMedicoViewModel.cs
+++ ControlMedico/ControlMedico/ViewModel/PrincipalMedicoViewModel.cs
@@ -1,10 +1,12 @@
 ﻿using CommunityToolkit.Mvvm.Input;
 using ControlMedico.Data.Model;
+using ControlMedico.Data.Repository;
 using ControlMedico.Model;
 using System;
 using System.Collections.Generic;
 using System.Globalization;
 using System.Text;
+using System.Threading.Tasks;
 using System.Windows.Input;
 using Xamarin.Forms;
 
@@ -15,10 +17,18 @@ namespace ControlMedico.ViewModel
     internal class PrincipalMedicoViewModel : BaseViewModel
     {
 
-        #region Attributes
+        #region AttributesPrincipal
         public CultureInfo CultureInfo => new CultureInfo("es-ES");
         private DateTime fechaSeleccionada = DateTime.Today;
         public Usuario usuario;
+        public List<Cita> Citas = new List<Cita>();
+        #endregion
+
+        #region AttributesCitaListView
+        public DateTime hora;
+        public string descripcion;
+        public bool isRefreshing = false;
+        public Object listViewSource;
         #endregion
 
         #region Properties
@@ -27,6 +37,32 @@ namespace ControlMedico.ViewModel
             get { return this.fechaSeleccionada; }
             set { SetValue(ref this.fechaSeleccionada, value); }
         }
+
+        public DateTime Hora
+        {
+            get { return this.hora; }
+            set { SetValue(ref this.hora, value); }
+        }
+
+        public string Descripcion
+        {
+            get { return this.descripcion; }
+            set { SetValue(ref this.descripcion, value); }
+        }
+
+        public bool IsRefreshing
+        {
+            get { return this.isRefreshing; }
+            set { SetValue(ref this.isRefreshing, value); }
+        }
+
+        public Object ListViewSource
+        {
+            get { return this.listViewSource; }
+            set { SetValue(ref this.listViewSource, value); }
+        }
+
+
         #endregion
 
         #region Commands
@@ -38,6 +74,11 @@ namespace ControlMedico.ViewModel
         #endregion
 
         #region Methods
+
+        public async Task CargarCitas()
+        {
+            this.ListViewSource = await CitaRepository.RecuperarCitasMedico(usuario.IdUsuario);
+        }
         private void ProbarFecha()
         {
             //Application.Current.MainPage.DisplayAlert("Fecha:",fechaSeleccionada.ToString("yyyy-MM-dd"),"Aceptar");
